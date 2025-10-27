6ee1c8c3ab518c83761f2fe76de9b7056f307089
socooly0410 <wofhddl0550@gmail.com>
2025-10-27 19:56:28 +0900

    Update Test.java
    연산자

--- Test.java
+++ Test.java
@@ -1,13 +1,22 @@
 package test;
+import java.util.Scanner;
+
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		int num1 = 7;
+		int num2 = 3;
 
+		int[] arr = {
+				num1 + num2,
+				num1 - num2,
+				num1 * num2,
+				num1 / num2
+		};
 
+		for (int i = 0; i <4 ; i++) {
 
-	}
-
-}
+					System.out.println(arr[i]);
+		}

d0a6f36ae1d85b4d0221e9efa239e748d304c76b
socooly0410 <wofhddl0550@gmail.com>
2025-10-27 20:01:44 +0900

    프로젝트

---
+++ study/src/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+import java.util.Scanner;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		
+		System.out.print("정수입력: ");
+
+
+
+
+	}
+
+}
